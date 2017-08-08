993aceb1d3844292ff3c0e75bd67c4974d2973b0
Ben Lamptey <ben@codepulse.co.uk>
2017-08-08 16:38:58 +0100

    OWL WORKS!

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -775,3 +775,7 @@ $databases['default']['default'] = array (
 );
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = '../config/sync';
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+ include $app_root . '/' . $site_path . '/settings.local.php';
+}
